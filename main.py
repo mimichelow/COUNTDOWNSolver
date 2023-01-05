4aad585b7705e18cf1e94f20b87d4ad2d68527c1
M. Michelow <mimichelow@uc.cl>
2023-01-04 21:50:21 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,29 @@
+from Trie import Trie
+
+#countdownSolver finds all the word that can be made using a 9 letter long string, so as to work as a solver for the
+# gameshow COUNTDOWN, it does this by generating a Trie from all the permutations, and iterating
+#the whole dictionary through it
+def countdownSolver(letters):
+    def permute(word):
+        if len(word) == 1:
+            return [word]
+
+        permutations = []
+        for i, letter in enumerate(word):
+            remaining_word = word[:i] + word[i + 1:]
+            for permutation in permute(remaining_word):
+                permutations.append(letter + permutation)
+        return permutations
+    letters=letters.upper()
+    t = Trie()
+    for end in permute(letters):
+        t.insert(end)
+    answer = []
+
+    with open("dictionary.txt") as file:
+        while (line := file.readline()):
+            line = line.strip()
+            if t.search(line):
+                answer.append(line)
+    #The answer is returned with the highest scoring words first
+    return(reversed(sorted(answer,key=lambda x:len(x))))
